707be6f9d68afd3738d670496e0735e5329f7f37
sub-2819 <subbiahkece@gmail.com>
2025-07-19 11:14:04 +0530

    Calculator.java

---
+++ Calculator/Calculator.java
@@ -0,0 +1,34 @@
+
+import java.util.*;
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter 1st number: ");
+        int a = sc.nextInt();sc.nextLine();
+        System.out.print("Enter 2nd number: ");
+        int b = sc.nextInt();sc.nextLine();
+        System.out.print("Enter operator: ");
+        char op = sc.nextLine().charAt(0);// character helps in future for comparing
+
+        if(op == '+')
+        {
+            System.out.println(a+" "+op+" "+b+" = "+(a+b));
+        }
+        else if(op == '-')
+        {
+            System.out.println(a+" "+op+" "+b+" = "+(a-b));
+        }
+        else if(op == '*')
+        {
+            System.out.println(a+" "+op+" "+b+" = "+(a*b));
+        }
+        else if(op == '/')
+        {
+            System.out.println(a+" "+op+" "+b+" = "+(a/b));
+        }
+        else
+        {
+            System.out.println("Invalid operator");
+        }
+    }
+}
